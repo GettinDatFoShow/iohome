f6d4dc38c368b06722956823c5d76561596ae720
Robert Morris <robert.dale.morris@gmail.com>
2018-10-23 23:46:56 -0400

    !

--- src/app/app.properties.ts
+++
@@ -1,9 +0,0 @@
-export const PRIVATE_CONFIG = {
-    apiKey: "AIzaSyBeAhHNhRyVhVwN7V63zTC6iJZHzryiNuI",
-    authDomain: "iohome-19fec.firebaseapp.com",
-    databaseURL: "https://iohome-19fec.firebaseio.com",
-    projectId: "iohome-19fec",
-    storageBucket: "iohome-19fec.appspot.com",
-    messagingSenderId: "869235653205"
-}
-
